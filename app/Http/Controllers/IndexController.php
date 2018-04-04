92a4b6a333b2f037a4c209b356112fd0329921d9
zjwlgr <zhangjian@kuaxue.com>
2018-04-04 17:32:06 +0800

    s

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,9 +18,38 @@ class IndexController extends Controller {
     }
 
     public function facade1(){
-        $users = DB::table('function')->select('id', 'fname')->get();
-        
-        dd($users);
+        echo '<pre>';
+        //$users = DB::table('function')->select('id', 'fname')->get();
+        /*$user = DB::table('function')
+            ->where('id',13)
+            ->select('id', 'fname')
+            ->first();*/
+
+        /*$fname = DB::table('function')
+            ->whereRaw('id >= ? and candel = ?', [18, 1])
+            ->pluck('fname', 'id');*/
+        //dd($fname);
+
+        /*echo '<pre>';
+        DB::table('function')->orderBy('id')->chunk(2, function ($tion) {
+            foreach ($tion as $key => $val) {
+                if ($val->fname == '管理员管理') {
+                    return false;
+                }
+                var_dump($val->fname);
+            }
+        });*/
+
+        /*$bool = DB::table('function')->where('id', 12)->exists();
+        var_dump($bool);*/
+
+        /*$first = DB::table('function')->where('id', 12)->value('fname');
+        var_dump($first);*/
+
+        $query = DB::table('function')->select('fid','fname')->distinct()->get();
+        $users = $query->addSelect('id')->get();
+        var_dump($users);
+
     }
 
 }
